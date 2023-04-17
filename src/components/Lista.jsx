a01913b75886e0999ee9a58a85b145950655b9dd
ivo2018 <ivo_argentina_07@hotmail.com>
2023-04-17 20:17:22 -0300

    a

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,9 +1,12 @@
+import Desayuno from "../imgs/desayuno.jpg";
+import PanDeMiga from "../imgs/Pan Miga.png";
+import Torta from "../imgs/Torta.jpg";
 const products = [
   {
     category: "platolisto",
     title: "pan de miga",
     price: 500,
-    img: "http://molinocarhue.com/wp-content/uploads/2017/11/pan-de-miga-blanco-1920x1293-73.png",
+    img: PanDeMiga,
   },
   {
     category: "platolisto",
@@ -15,37 +18,13 @@ const products = [
     category: "pasteleria",
     title: "tortas",
     price: 5000,
-    img: "http://www.lareinapinamar.com.ar/img/inicio/1.jpg",
+    img: Torta,
   },
   {
     category: "confiteria",
     title: "desayuno",
     price: 3000,
-    img: "http://www.lareinapinamar.com.ar/img/inicio/3.jpg",
-  },
-  {
-    category: "platolisto",
-    title: "pan de miga",
-    price: 500,
-    img: "http://molinocarhue.com/wp-content/uploads/2017/11/pan-de-miga-blanco-1920x1293-73.png",
-  },
-  {
-    category: "platolisto",
-    title: "pan lactal",
-    price: 500,
-    img: "https://productosfrontera.com.ar/wp-content/uploads/2020/02/PAN-LACTAL-BLANCO.jpg",
-  },
-  {
-    category: "pasteleria",
-    title: "tortas",
-    price: 5000,
-    img: "http://www.lareinapinamar.com.ar/img/inicio/1.jpg",
-  },
-  {
-    category: "confiteria",
-    title: "desayuno",
-    price: 3000,
-    img: "http://www.lareinapinamar.com.ar/img/inicio/3.jpg",
+    img: Desayuno,
   },
 ];
 export default products;
