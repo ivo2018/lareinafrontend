ffb276598b83d1e69038b460cbde775b338a53e9
ivo2018 <ivo_argentina_07@hotmail.com>
2023-04-17 20:11:24 -0300

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./Home.css";
+import Principal from "../imgs/ReinaPrincipal.jpg";
+import Principal1 from "../imgs/ReinaPrincipal1.jpg";
+import Principal2 from "../imgs/ReinaPrincipal2.jpg";
 function Home() {
   return (
     <div class="Home-Carousel">
       <div class="Home-Carousel__principal">
-        <img
-          src="http://www.lareinapinamar.com.ar/img/inicio/2.jpg"
-          alt="la reina"
-        />
+        <img src={Principal} alt="la reina" />
       </div>
       <div class="Home-Carousel__cards">
         <div class="Home-Carousel__second">
           <p>La Reina Pinamar</p>
-          <img
-            src="http://www.lareinapinamar.com.ar/img/inicio/3.jpg"
-            alt="la reina"
-          />
+          <img src={Principal1} alt="la reina" />
           <b>
             40 AÑOS DE HISTORIA EN EL RUBRO, BRINDANDNO PRODUCTOS DE EXCELENCIA.
             VISITE LA CASA CENTRAL EN PINAMAR.
@@ -23,10 +20,7 @@ function Home() {
         </div>
         <div class="Home-Carousel__trhee">
           <p>Servicio Mayorista</p>
-          <img
-            src="http://www.lareinapinamar.com.ar/img/thumb_mayorista.jpg"
-            alt="la reina"
-          />
+          <img src={Principal2} alt="la reina" />
           <b>
             NUESTRA FÁBRICA EN PINAMAR PROVEE PRODUCTOS A EMPRESAS Y LOCALES DE
             TODA LA COSTA ATLÁNTICA.
